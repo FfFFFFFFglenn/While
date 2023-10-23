354eaba4764756264658a2f13a88d4c865c5b7c3
FfFFFFFFglenn <145317870+FfFFFFFFglenn@users.noreply.github.com>
2023-10-23 13:41:52 +0800

    Add files via upload

--- JavaApplication21.java
+++ JavaApplication21.java
@@ -21,6 +21,8 @@ public class JavaApplication21 {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a Number:");
         int a = input.nextInt();
+        System.out.println ("The of a prime number below or equal to:" + a);
+        System.out.println ("0");
 
         int sum = 0;
         for (int i = 2; i <= a; i++) {
@@ -33,10 +35,10 @@ public class JavaApplication21 {
             }
 
             if (prime) {
-                System.out.println(i + " is prime" );
+                //System.out.println("+ "+ i + " is prime" );
                 sum = sum + i;
 
-                System.out.println("The sum of prime no. below or equal to " + i + " = " + sum);
+                System.out.println("The sum is " + i + " = " + sum);
             }
 
         }
