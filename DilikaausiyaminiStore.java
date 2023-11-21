0bd0a5bb72e1dffa115c68477948ef60f589861b
Dubuuu <145317870+chewychae@users.noreply.github.com>
2023-11-21 22:52:14 +0800

    Update DilikaausiyaminiStore.java

--- DilikaausiyaminiStore.java
+++ DilikaausiyaminiStore.java
@@ -1,122 +1,123 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package dilikaausiyamini.store;
-
-import javax.swing.JOptionPane;
-
+package com.mycompany.glennstore;
 /**
  *
- * @author Fedrequilan_CPE
+ * @author Acer
  */
+import javax.swing.JOptionPane;
+
 public class DilikaausiyaminiStore {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
-        String YOURNAME = JOptionPane.showInputDialog("Welcome to the Dili Kaayo Siya Mini Store!\nName:");
-        if (YOURNAME == null) {
-            JOptionPane.showMessageDialog(null, "Okaaaaayyyyy Byeeee!!");
-            System.exit(0);
-        }
-
-        String[] items = {"Flashlight - P125.50", "Boots - P312", "Umbrella - P263", "Raincoat- P255 ", "Bag - P999"};
-        double[] prices = {125.50, 312.0, 263.0, 255.0, 999.0};
-        double totalCost = 0.00;
-        double paramakadiscount2 = 1800.0; //Pila ang dapat mabayran para maka avail ug discount 2.
-        double discount2 = 0.15; //15%
-
-        StringBuilder receipt = new StringBuilder("Receipt for " + YOURNAME + "\n");
-
         while (true) {
-            int choice = JOptionPane.showOptionDialog(null, "Pagpili ug gusto nimo na item " + YOURNAME, "Dili Kaayo Ni Siya Mini Store",
-                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, items, items[0]);
-
-            if (choice == -1) {
+            String YOURNAME = JOptionPane.showInputDialog("Welcome to the Dili Kaayo Siya Mini Store!\nName:");
+            if (YOURNAME == null) {
                 JOptionPane.showMessageDialog(null, "Okaaaaayyyyy Byeeee!!");
                 System.exit(0);
-                break; // Pag i cancel
             }
 
-            int quantity;
+            String[] items = {"Flashlight - P125.50", "Boots - P312", "Umbrella - P263", "Raincoat- P255 ", "Bag - P999"};
+            double[] prices = {125.50, 312.0, 263.0, 255.0, 999.0};
+            double totalCost = 0.00;
+            double paramakadiscount2 = 1800.0; // Pila ang dapat mabayran para maka avail ug discount 2.
+            double discount2 = 0.15; // 15%
+
+            StringBuilder receipt = new StringBuilder("Receipt for " + YOURNAME + "\n");
+
             while (true) {
-                try {
+                int choice = JOptionPane.showOptionDialog(null, "Pagpili ug gusto nimo na item " + YOURNAME,
+                        "Dili Kaayo Ni Siya Mini Store", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
+                        null, items, items[0]);
+
+                if (choice == -1) {
+                    JOptionPane.showMessageDialog(null, "Okaaaaayyyyy Byeeee!!");
+                    System.exit(0);
+                }
+
+                int quantity = 0;
+                boolean isValidQuantity = false;
+                while (!isValidQuantity) {
                     String quantityString = JOptionPane.showInputDialog("Enter quantity for " + items[choice]);
                     if (quantityString == null) {
                         JOptionPane.showMessageDialog(null, "Okaaaaayyyyy Byeeee!!");
                         System.exit(0);
                     }
-
-                    quantity = Integer.parseInt(quantityString);
-
-                    if (quantity < 0) {
-                        throw new NumberFormatException();
+                    if (quantityString.matches("\\d+")) {
+                        quantity = Integer.parseInt(quantityString);
+                        if (quantity > 0) {
+                            isValidQuantity = true;
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Please enter a valid positive whole number for the quantity.", "Invalid Quantity", JOptionPane.WARNING_MESSAGE);
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Please enter a valid positive whole number for the quantity.", "Invalid Quantity", JOptionPane.WARNING_MESSAGE);
                     }
+                }
 
-                    if (quantityString.contains(".")) {
-                        throw new NumberFormatException();
-                    }
+                double itemCost = prices[choice] * quantity;
+                totalCost += itemCost;
 
-                    break; // Exit ang loop pag satisfy ang condition
-                } catch (NumberFormatException e) {
-                    JOptionPane.showMessageDialog(null, "WHOLE NUMBER LANG ANG PWEDE.", "OPPSSSS DILI NA PWEDE", JOptionPane.WARNING_MESSAGE);
+                receipt.append(items[choice]).append(" x ").append(quantity).append(": P").append(itemCost).append("\n");
+
+                int addAnotherItem = JOptionPane.showConfirmDialog(null, "Do you want to add another item?", "Add Item",
+                        JOptionPane.YES_NO_OPTION);
+
+                if (addAnotherItem == JOptionPane.NO_OPTION) {
+                    break;
                 }
             }
 
-            double itemCost = prices[choice] * quantity;
-            totalCost += itemCost;
+            if (totalCost >= paramakadiscount2) {
+                double discountAmount = totalCost * discount2;
+                totalCost -= discountAmount;
+                receipt.append("Discount (15%): -P").append(discountAmount).append("\n");
 
-            receipt.append(items[choice]).append(" x ").append(quantity).append(": P").append(itemCost).append("\n");
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Congratulations! Maka discount kag 15%, kay nakaabot/nalapas kag P" + paramakadiscount2,
+                        "Discount Offer",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
 
-            int addAnotherItem = JOptionPane.showConfirmDialog(null, "Do you want to add another item?", "Add Item",
+            String paymentString;
+            double payment;
+            while (true) {
+                paymentString = JOptionPane.showInputDialog("Your Total Cost is: P" + totalCost + "\nEnter payment amount (P):");
+                if (paymentString == null) {
+                    JOptionPane.showMessageDialog(null, "Okaaaaayyyyy Byeeee!!");
+                    System.exit(0);
+                }
+                payment = Double.parseDouble(paymentString);
+                if (payment >= totalCost) {
+                    break;
+                } else {
+                    int addMoreMoney = JOptionPane.showConfirmDialog(null, "Kulang imong bayad, " + YOURNAME + ". Mag-add kag bayad?", "HOYYYY!!",
+                            JOptionPane.YES_NO_OPTION);
+                    if (addMoreMoney == JOptionPane.NO_OPTION) {
+                        JOptionPane.showMessageDialog(null, "Kulang jud imong bayad. Sorryyy Byeeee!!");
+                        System.exit(0);
+                    }
+                }
+            }
+
+            double change = payment - totalCost;
+
+            receipt.append("Total Cost: P").append(totalCost).append("\n");
+            receipt.append("Payment: P").append(payment).append("\n");
+            receipt.append("Change: P").append(change);
+
+            JOptionPane.showMessageDialog(null, receipt.toString(), "Mini Store Receipt", JOptionPane.INFORMATION_MESSAGE);
+
+            int continuePurchase = JOptionPane.showConfirmDialog(null, "Do you want to make another purchase?", "Continue",
                     JOptionPane.YES_NO_OPTION);
 
-            if (addAnotherItem == JOptionPane.NO_OPTION) {
-                break; // Dli na ganahan pa mu add
+            if (continuePurchase == JOptionPane.NO_OPTION) {
+                JOptionPane.showMessageDialog(null, "Thank you!!!!! Sunod na sad!!");
+                System.exit(0);
             }
         }
-
-        if (totalCost >= paramakadiscount2) {
-            double discountAmount = totalCost * discount2;
-            totalCost -= discountAmount;
-            receipt.append("Discount (15%): -P").append(discountAmount).append("\n");
-
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Congratulations! Maka discount kag 15%, kay nakaabot/nalapas kag P" + paramakadiscount2,
-                    "Discount Offer",
-                    JOptionPane.INFORMATION_MESSAGE);
-        }
-
-        String paymentString = JOptionPane.showInputDialog("Your Total Cost is:P" + totalCost + " \nEnter payment amount (P):");
-        if (paymentString == null) {
-            JOptionPane.showMessageDialog(null, "Okaaaaayyyyy Byeeee!!");
-            System.exit(0);
-        }
-        double payment = Double.parseDouble(paymentString);
-
-        double change = payment - totalCost;
-
-        if (change < 0) {
-            JOptionPane.showMessageDialog(null, "Kulang imong kwarta byeeeee.", "Payment Error",
-                    JOptionPane.ERROR_MESSAGE);
-        } else if (change > 0) {
-            receipt.append("Total Cost: P").append(totalCost).append("\n");
-            receipt.append("Payment: P").append(payment).append("\n");
-            receipt.append("Change: P").append(change);
-
-            JOptionPane.showMessageDialog(null, receipt.toString(), "Mini Store Receipt", JOptionPane.INFORMATION_MESSAGE);
-        } else if (change == 0) {
-
-            receipt.append("Total Cost: P").append(totalCost).append("\n");
-            receipt.append("Payment: P").append(payment).append("\n");
-            receipt.append("Change: P").append(change);
-            JOptionPane.showMessageDialog(null, receipt.toString(), "Mini Store Receipt", JOptionPane.INFORMATION_MESSAGE);
-        }
-        // TODO code application logic here
     }
 }
